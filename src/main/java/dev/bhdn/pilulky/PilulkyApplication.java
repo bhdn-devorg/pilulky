89d54fa0a7511b963ab1daf8e76dae498132b6ab
Bohdan Hovorukha <bohdan.hovorukha@gmail.com>
2025-09-15 01:40:39 +0200

    first commit

---
+++ src/main/java/dev/bhdn/pilulky/PilulkyApplication.java
@@ -0,0 +1,13 @@
+package dev.bhdn.pilulky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilulkyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PilulkyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/bhdn/pilulky/PilulkyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bhdn.pilulky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilulkyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
